f27a5b9831f39c82bc7b3c490f0f085cc301ab50
screret <68943070+screret@users.noreply.github.com>
2024-04-23 16:51:28 +0300

    try to fix #728

--- src/main/java/com/gregtechceu/gtceu/client/renderer/BlockHighLightRenderer.java
+++ src/main/java/com/gregtechceu/gtceu/client/renderer/BlockHighLightRenderer.java
@@ -10,17 +10,20 @@ import com.gregtechceu.gtceu.api.item.PipeBlockItem;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.api.item.tool.IToolGridHighLight;
 import com.gregtechceu.gtceu.api.item.tool.ToolHelper;
+import com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiController;
 import com.gregtechceu.gtceu.api.pipenet.IPipeType;
 import com.gregtechceu.gtceu.common.item.CoverPlaceBehavior;
 import com.gregtechceu.gtceu.common.item.tool.rotation.CustomBlockRotations;
 import com.gregtechceu.gtceu.common.item.tool.rotation.ICustomRotationBehavior;
 import com.gregtechceu.gtceu.core.mixins.GuiGraphicsAccessor;
+import com.gregtechceu.gtceu.core.mixins.LocalPlayerAccessor;
 import com.lowdragmc.lowdraglib.client.utils.RenderUtils;
 import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Transformation;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -38,6 +41,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
+import org.lwjgl.opengl.GL11;
 
 import java.util.Set;
 import java.util.function.Function;
@@ -62,11 +66,15 @@ public class BlockHighLightRenderer {
             Set<GTToolType> toolType = ToolHelper.getToolTypes(held);
             BlockEntity blockEntity = level.getBlockEntity(blockPos);
 
+            double d3 = ((LocalPlayerAccessor)player).getXLast() + (player.position().x - ((LocalPlayerAccessor)player).getXLast()) * (double) partialTick;
+            double d4 = ((LocalPlayerAccessor)player).getYLast() + (player.position().y - ((LocalPlayerAccessor)player).getYLast()) * (double) partialTick;
+            double d5 = ((LocalPlayerAccessor)player).getZLast() + (player.position().z - ((LocalPlayerAccessor)player).getZLast()) * (double) partialTick;
+
             // draw tool grid highlight
             if (!toolType.isEmpty() && blockEntity instanceof IToolGridHighLight gridHighLight) {
                 Vec3 pos = camera.getPosition();
                 poseStack.pushPose();
-                poseStack.translate(-pos.x, -pos.y, -pos.z);
+                poseStack.translate(-pos.x + d3, -pos.y + d4, -pos.z + d5);
                 if (gridHighLight.shouldRenderGrid(player, held, toolType)) {
                     var buffer = multiBufferSource.getBuffer(RenderType.lines());
                     RenderSystem.lineWidth(3);
